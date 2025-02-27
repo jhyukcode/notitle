e1aa2a376ea3c5552c919f1ba3cb20cb23d7c7f3
AA\User <utt7418@gmail.com>
2025-02-28 03:02:16 +0900

    2025-02-28 commit

---
+++ JAVA/java/src/com/company/java008_ex/MethodEx006.java
@@ -0,0 +1,44 @@
+package com.company.java008_ex;
+
+public class MethodEx006 {
+
+	public static void main(String[] args) {
+
+//		
+//		public class MethodEx006{ 
+//			   // 변수
+//			   String [][] users = {{ "aaa"  , "111"   , "한국"     } ,     // 00 01 02
+//			                      { "bbb" , "222"   , "호주"       } ,       // 10 11 12
+//			                      { "ccc"  , "333"   , "중국"      }};      // 20 21 22
+//			   // 입력
+//			   who_are_you(users);    
+//			   //  public static void who_are_you(String [][] users){  아이디를입력받아서 나라조회 }
+//			   who_pass_change(users); 
+//			   //  public static void who_pass_change(String [][] users){ 아이디,비밀번호가 맞으면 비밀번호 바꾸기}
+//			}
+//
+//
+//			출력내용) who_are_you(users);    
+//			//  public static void who_are_you(String [][] users){  아이디를입력받아서 나라조회 }
+//			아이디를 입력해주세요>aaa
+//			aaa는 한국사람입니다.
+//
+//
+//			출력내용)
+//			who_pass_change(users); 
+//			//  public static void who_pass_change(String [][] users){ 아이디,비밀번호가 맞으면 비밀번호 바꾸기}
+//
+//			아이디를 입력해 주세요 > bbb
+//			비밀번호를 입력해 주세요 > 123
+//			유저를 확인해주세요!
+//
+//			아이디를 입력해 주세요 > bbb
+//			비밀번호를 입력해 주세요 > 222
+//			변경하실 비밀번호를 입력해주세요123
+//			정보확인 : [bbb, 123, 호주]
+//		
+		
+		
+	}
+
+}
