9b38f81e97dd8056dc8f7790a4f821a90e16e89f
AA\User <utt7418@gmail.com>
2025-04-11 02:39:37 +0900

    간단한 작업

--- SPRINGBOOT/boot000/src/main/java/com/company/boot000/member/Member.java
+++ SPRINGBOOT/boot000/src/main/java/com/company/boot000/member/Member.java
@@ -11,8 +11,9 @@ import javax.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @ToString
 public class Member {
 	/* 필수정보 */
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
